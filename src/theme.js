70c8ccd1ac581a38136ca5dd8eea3476790c664f
Marino Imola <marinoimola27@gmail.com>
2020-12-05 02:03:34 +0700

    remove unused import

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { blue, grey, orange, red } from '@material-ui/core/colors';
+import { blue, orange } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
   palette: {
